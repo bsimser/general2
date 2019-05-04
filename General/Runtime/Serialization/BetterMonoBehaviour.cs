68dd6c7c58203e1e13698c2625786d092022afde
Joris Huijbregts <jjahuijbregts@gmail.com>
2019-05-04 18:47:15 +0200

    JsonSerializerObsolete now loads data from fullserializer and lets Odin re-serialize it, switching to Odin that way

--- General/Runtime/Serialization/BetterMonoBehaviour.cs
+++ General/Runtime/Serialization/BetterMonoBehaviour.cs
@@ -7,6 +7,33 @@ namespace Devdog.General2
 {
     public class BetterMonoBehaviour : SerializedMonoBehaviour
     {
-       
+        /// <summary>
+        /// Used to store the state of this scriptable object. 
+        /// Fetched through reflection to avoid people messing with it.
+        /// </summary>
+        [SerializeField]
+        [IgnoreCustomSerialization] // Ignore in custom serializer - Let unity seriarlize this.
+        private string _serializedJsonString = "{}";
+
+        [SerializeField]
+        [IgnoreCustomSerialization] // Ignore in custom serializer - Let unity seriarlize this.
+        private List<UnityEngine.Object> _objectReferences;
+
+        [NonSerialized]
+        private readonly BetterSerializationModel _serializer = new BetterSerializationModel();
+
+        public virtual void Load()
+        {
+            _serializer.Load(ref _objectReferences, ref _serializedJsonString, this);
+        }
+        protected override void OnAfterDeserialize()
+        {
+            if (string.IsNullOrEmpty(_serializedJsonString) == false && _serializedJsonString.Replace(" ", "") != "{}")
+            {
+                Load();
+                _serializedJsonString = "";
+                _objectReferences = new List<UnityEngine.Object>();
+            }
+        }
     }
 }
--- General/Runtime/Serialization/BetterSerializationModel.cs
+++ General/Runtime/Serialization/BetterSerializationModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿#pragma warning disable 612
+
+using System;
 using System.Collections.Generic;
 using System.Text;
 using UnityEngine;
@@ -10,32 +12,21 @@ namespace Devdog.General2
         [NonSerialized]
         private bool _isSerializing = false;
 
-        public void Save<T>(ref List<UnityEngine.Object> objectReferences, ref byte[] json, T obj)
-            where T: UnityEngine.Object
-        {
-            if (_isSerializing || Application.isPlaying)
-                return;
+//        public void Save(ref List<UnityEngine.Object> objectReferences, ref string json, UnityEngine.Object obj)
+//        {
+//            // Ignored; Odin takes over
+//        }
 
-            _isSerializing = true;
-            objectReferences = new List<UnityEngine.Object>(); // Has to be new list, ref type -> Clear will clear it inside the serializer
-            json = JsonSerializer.SerializeObject(obj, objectReferences);
-
-#if UNITY_EDITOR
-            UnityEditor.EditorUtility.SetDirty(obj);
-#endif
-
-            _isSerializing = false;
-        }
-
-        public void Load<T>(ref List<UnityEngine.Object> objectReferences, ref byte[] json, T deserializeTo)
-            where T: UnityEngine.Object
+        public void Load(ref List<UnityEngine.Object> objectReferences, ref string json, UnityEngine.Object obj)
         {
             if (_isSerializing)
                 return;
 
             _isSerializing = true;
-            JsonSerializer.DeserializeObject(deserializeTo, json, objectReferences);
+            JsonSerializerObsolete.DeserializeTo(obj, obj.GetType(), json, objectReferences);
             _isSerializing = false;
         }
     }
 }
+
+#pragma warning restore 612
